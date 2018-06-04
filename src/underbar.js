4d66e7883ba36e7ba2ae9b631009a5c0fda4e018
inh556 <yanbin.jin@gmail.com>
2018-06-04 13:55:01 -0700

    implemented extend function

--- src/underbar.js
+++ src/underbar.js
@@ -264,6 +264,12 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for(let argument of arguments) {
+      for(let item in argument) {
+        obj[item] = argument[item];
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
