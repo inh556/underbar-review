5f39e79c6fd8e2d51023cdb3591c7022fd9b0d8f
inh556 <yanbin.jin@gmail.com>
2018-06-10 21:12:44 -0700

    implemented invoke

--- src/underbar.js
+++ src/underbar.js
@@ -386,6 +386,16 @@
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    return _.map(collection, function(item) {
+      if(typeof functionOrKey === 'string') {
+        //transoform string to method 
+        //String.prototype.toLocaleUpperCase() 
+        // using bracket notation becasue the metond passed in is a 'string'
+        return String.prototype[functionOrKey].apply(item,args);
+      } else {
+        return functionOrKey.apply(item, args);
+      }
+    });
   };
 
   // Sort the object's values by a criterion produced by an iterator.
